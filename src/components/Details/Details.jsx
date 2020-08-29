377526f3e4387effdcb460adf92e6510ec80f235
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-29 10:26:06 -0500

    fix display for multiple categories

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -13,13 +13,6 @@ class Details extends Component {
 
     render() {
 
-//         details: Array(1)
-// 0:
-// description: "Avatar (marketed as James Cameron's Avatar) is a 2009 American epic science fiction film directed, written, produced, and co-edited by James Cameron, and stars Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez, and Sigourney Weaver. The film is set in the mid-22nd century, when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the mineral unobtanium, a room-temperature superconductor. The expansion of the mining colony threatens the continued existence of a local tribe of Na'vi – a humanoid species indigenous to Pandora. The film's title refers to a genetically engineered Na'vi body operated from the brain of a remotely located human that is used to interact with the natives of Pandora."
-// id: 1
-// name: "Fantasy"
-// poster: "images/avatar.jpeg"
-// title: "Avatar
         return (
             <>
                 <h2>Details</h2>
@@ -27,10 +20,19 @@ class Details extends Component {
         <div className='details'>
             <img src={this.props.reduxState.details[0].poster} alt={this.props.reduxState.details[0].title} />
         <h3>{this.props.reduxState.details[0].title}</h3>
-        <h4>Categories: {this.props.reduxState.details[0].name}</h4>
+        <h4>Categories:</h4>
+        <ul>
+            {this.props.reduxState.details.map((item) => {
+                return (
+                    <li>{item.name}</li>
+                )
+            })}
+
+
+        </ul>
         <p>{this.props.reduxState.details[0].description}</p>
         </div>}
-        
+
                 <button onClick={() => this.props.history.push('/')}>Back</button>
             </>
 
